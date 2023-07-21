df47160ffbed46b7ad2dcb88cb73a46d4475a2ad
Meet Panara <136955195+Meet-Panara@users.noreply.github.com>
2023-07-21 15:50:02 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,5 @@
 <?php
-
+//Need to create the sql Database Called 'shop_db' for refrence visit the folder 'sql-file' in this repo
 $conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
 
 ?>
